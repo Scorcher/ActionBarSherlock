352419098f8d6b5a6d50a2cdf42d6fc99462e287
Rogov Alexander <rogovalex@yandex.ru>
2015-10-21 09:36:20 +0500

    * для формирования меню теперь используются только добавленные фрагменты

--- actionbarsherlock/src/android/support/v4/app/Watson.java
+++ actionbarsherlock/src/android/support/v4/app/Watson.java
@@ -54,7 +54,8 @@ public abstract class Watson extends FragmentActivity implements OnCreatePanelMe
             if (fragments != null) {
                 for (int i = 0; i < fragments.size(); i++) {
                     Fragment f = fragments.get(i);
-                    if (f != null && !f.mHidden && f.mHasMenu && f.mMenuVisible && f instanceof OnCreateOptionsMenuListener) {
+                    if (f != null && f.mAdded && !f.mHidden && f.mHasMenu
+                            && f.mMenuVisible && f instanceof OnCreateOptionsMenuListener) {
                         show = true;
                         ((OnCreateOptionsMenuListener)f).onCreateOptionsMenu(menu, inflater);
                         if (newMenus == null) {
@@ -98,7 +99,8 @@ public abstract class Watson extends FragmentActivity implements OnCreatePanelMe
             if (fragments != null) {
                 for (int i = 0; i < fragments.size(); i++) {
                     Fragment f = fragments.get(i);
-                    if (f != null && !f.mHidden && f.mHasMenu && f.mMenuVisible && f instanceof OnPrepareOptionsMenuListener) {
+                    if (f != null && f.mAdded && !f.mHidden && f.mHasMenu
+                            && f.mMenuVisible && f instanceof OnPrepareOptionsMenuListener) {
                         show = true;
                         ((OnPrepareOptionsMenuListener)f).onPrepareOptionsMenu(menu);
                     }
@@ -128,7 +130,8 @@ public abstract class Watson extends FragmentActivity implements OnCreatePanelMe
             if (fragments != null) {
                 for (int i = 0; i < fragments.size(); i++) {
                     Fragment f = fragments.get(i);
-                    if (f != null && !f.mHidden && f.mHasMenu && f.mMenuVisible && f instanceof OnOptionsItemSelectedListener) {
+                    if (f != null && f.mAdded && !f.mHidden && f.mHasMenu
+                            && f.mMenuVisible && f instanceof OnOptionsItemSelectedListener) {
                         if (((OnOptionsItemSelectedListener)f).onOptionsItemSelected(item)) {
                             return true;
                         }
